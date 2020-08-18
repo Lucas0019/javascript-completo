d40a8ef7a69cf4c74bdc2e47ff7daff6d857b544
Lucas <lxavier.00@outlook.com>
2020-08-18 18:39:11 -0300

    Aula 03 concluida

--- 02-Base-JavaScript/03_Boolean-e-condicionais.js
+++ 02-Base-JavaScript/03_Boolean-e-condicionais.js
@@ -65,3 +65,44 @@ false || true; // true
 (5 - 5) || (5 + 5); // 10
 'Gato' || false; // Gato
 (5 >= 5) || (3 < 6); // true
+
+//Teste 
+
+var cond = (5 - 5) || (5 + 5) && (10 - 2)
+
+console.log(cond);
+
+
+//SWITCH
+//Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra chave break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira.
+
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+    case 'Azul':
+        console.log('Olhe para o céu.');
+        break;
+    case 'Vermelho':
+        console.log('Olhe para rosas.');
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol.');
+        break;
+    default:
+        console.log('Feche os olhos');
+}
+
+//Aplicando o conhecimento 
+let valorNasc = 2000
+
+switch (valorNasc) {
+    case 2020:
+        console.log('Não vim do futuro ');
+        break;
+    case 2000:
+        console.log('Acho minha data de nascimento');
+        break;
+    default:
+        console.log('Data invalida');
+        break;
+}
