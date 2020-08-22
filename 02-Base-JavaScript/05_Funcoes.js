c06d26b568bb30b93159b341636db26bab3bd0c8
Lucas <lxavier.00@outlook.com>
2020-08-22 18:08:37 -0300

    Aula 05 funçoes ok

--- 02-Base-JavaScript/05_Funcoes.js
+++ 02-Base-JavaScript/05_Funcoes.js
@@ -1,8 +1,69 @@
-//Chamada de function declaration
-
-function areaQuadrado(base, altura) {
-    return base * altura;
+//function declaration
+function areaQuadrado(lado) {
+    console.log(lado * lado);
 }
 
+areaQuadrado(2) //4
+areaQuadrado(4) //16
+areaQuadrado(8) //64
 
-console.log('A area do quadrado é: ' + areaQuadrado(2, 2));
+//Declarando e executando uma função
+
+function pi() {
+    return 3.14;
+}
+
+var total = 5 * pi()
+console.log(total.toFixed(2)); //15.70
+
+//Parâmetros e Argumentos
+//Ao criar uma função, você pode definir parâmetros.
+//Ao executar uma função, você pode passar argumentos.
+
+function imc(pessoa, altura) {
+    const imc = pessoa / (altura ** 2); //altura * altura
+    console.log(imc.toFixed(2));
+}
+
+// 80 e 1.80 são os argumentos
+imc(80, 1.80) //24.69
+imc(70, 1.78) //22.09
+
+
+//Parênteses executa a função
+
+function corFavorita(cor) {
+    if (cor === 'Azul') {
+        console.log('Voce gosta do céu');
+    } else if (cor === 'Verde') {
+        console.log('Voce gosta do mato');
+    } else {
+        console.log('Voce não gosta de nada');;
+    }
+}
+
+corFavorita('Verde')
+corFavorita('Azul')
+corFavorita('Nada')
+
+//Argumentos podem ser funções
+//Chamadas de Callback, geralmente são funções que ocorrem após algum evento.
+//Executa no seu browser 
+// addEventListener('click', function() {
+//     console.log('clicou');
+// })
+
+//Funções anônimas são aquelas em que o nome da função não é definido, 
+//escritas como function() {} ou () => {}
+
+
+//Exemplo de outra função anonima através de um objeto
+const pessoa = {
+    falar: function() {
+        console.log("Opa Dev")
+    }
+
+}
+
+//chamando a funcao pessoa
+pessoa.falar()
