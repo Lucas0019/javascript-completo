434ee5542b5f33828593142fef67984987269299
Lucas <lxavier.00@outlook.com>
2020-08-23 16:00:35 -0300

    Aula 08 add

---
+++ 02-Base-JavaScript/08_Objetos.js
@@ -0,0 +1,14 @@
+//Objetos :
+//Conjunto de variáveis e funções, que são chamadas de propriedades e métodos.
+var pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Xavier',
+    idade: 20,
+    possuiFaculdade: true,
+}
+
+
+console.log(pessoa.nome); //Lucas
+console.log(pessoa.sobrenome); //Xavier
+console.log(pessoa.idade); //20
+console.log(pessoa.possuiFaculdade); //true
