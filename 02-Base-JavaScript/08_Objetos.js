9807c8ac59ea8d7044c64557b47d2ed91bc464be
Lucas <lxavier.00@outlook.com>
2020-08-25 20:09:29 -0300

    Aula 08 parte-02 ok

--- 02-Base-JavaScript/08_Objetos.js
+++ 02-Base-JavaScript/08_Objetos.js
@@ -74,3 +74,81 @@ console.table(quadrado)
 
 //Já percebeu que console é um objeto e
 // log() um método ?
+
+
+//Dot Notation Get - PEGAR
+//utilizando a notação ponto para acessar as propriedades 
+var menu = {
+    width: 800,
+    height: 50,
+    background: '#ffff'
+}
+
+var bg = menu.background;
+console.log(bg); //#ffff
+
+//Setando o objeto, mudou a cor do background
+menu.background = '#000'
+var bg = menu.background;
+console.log(bg);
+
+//Adicionando mais  atributos
+menu.color = 'blue'
+console.log(menu);
+// { width: 800, height: 50, background: '#000', color: 'blue' }
+
+menu.somaDois = function(a, b) {
+    return a + b
+}
+
+console.log(menu.somaDois(2, 2)); //4
+
+
+
+//Dot Notation Set 
+//Substitua o valor de uma propriedade utilizando . e o = após o nome da mesma.
+var pageConfig = {
+    width: 800,
+    height: 50,
+    background: '#84E'
+}
+
+pageConfig.background = '#000'
+console.log(pageConfig.background);
+
+//Add Propriedades e Métodos
+var euAqui = {
+    width: 900,
+}
+
+euAqui.height = 50
+euAqui.position = 'fixed';
+console.log(euAqui);
+
+
+//PALAVRA CHAVE - THIS
+//this irá fazer uma referência ao próprio objeto.
+
+var altura = 120
+var configHome = {
+    altura: 800,
+    largura: 50,
+    metadeLargura() {
+        return this.largura / 2;
+    }
+}
+
+console.log(configHome.metadeLargura()); //50
+//Sem o this, seria 60
+
+
+
+//Protótipo e Herança
+//O objeto herda propriedades e métodos do objeto que foi utilizado para criar o mesmo.
+var menu = {
+    width: 800,
+}
+
+menu.hasOwnProperty('width') // true
+menu.hasOwnProperty('height') // false
+    //hasOwnProperty é um método de Object
