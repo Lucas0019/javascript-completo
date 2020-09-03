429b94a6238e183cfb48a2bd8acf70dcb095d764
Lucas <lxavier.00@outlook.com>
2020-09-03 17:36:06 -0300

    Exerc Aula 03 - DOM

---
+++ 03-DOM-Iniciante/01-DOM/02_scriptExercicio.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const retornaUrlAtual = window.location.href
+console.log(retornaUrlAtual)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+
+
+// Retorne a linguagem do navegador
+const qualLing = window.navigator.language;
+
+
+// Retorne a largura da janela
+
+const qualLargura = window.innerWidth
