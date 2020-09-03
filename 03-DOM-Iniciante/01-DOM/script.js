8524460d0997ea400cd4ae5da0fe60bcdcd4a1ec
Lucas <lxavier.00@outlook.com>
2020-09-03 14:22:11 -0300

    Upload Aula 03 - DOM

---
+++ 03-DOM-Iniciante/01-DOM/script.js
@@ -0,0 +1,24 @@
+// window.alert('Eae dev')
+
+// const href = window.location.href
+// console.log(href); //'http://127.0.0.1:5500/03-DOM-Iniciante/01-DOM/'
+
+// if (href === 'http://127.0.0.1:5500/03-DOM-Iniciante/01-DOM/') {
+//     console.log(window.alert('É igual !!!'));
+// } else {
+//     console.log('É diferente');
+// }
+
+// document...
+
+
+// Window e Document
+// São os objetos principais do DOM, boa parte da manipulação é feita através dos seus métodos e propriedades.
+
+// window.alert('Isso é um alerta');
+// alert('Isso é um alerta'); // Funciona
+
+
+//Selecionar uma tag html
+const h1Selecionado = document.querySelector('h1'); // Seleciona o primeiro h1
+document.body; // Retorna o body
