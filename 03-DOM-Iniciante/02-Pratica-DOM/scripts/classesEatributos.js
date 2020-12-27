605c99fccd058287747c5c0ee39ce1cb05ebe569
Lucas <lxavier.00@outlook.com>
2020-12-26 21:59:23 -0300

    ☕ Modulo refeito

--- 03-DOM-Iniciante/02-Pratica-DOM/scripts/classesEatributos.js
+++ 03-DOM-Iniciante/02-Pratica-DOM/scripts/classesEatributos.js
@@ -1,23 +1,3 @@
 // CLASSLIST
     // Retorna uma lista com as classes do elemento. Permite adicionar, remover e verificar se contém.
 
-    const menu = document.querySelector('.menu');
-
-    menu.className; // string
-    menu.classList; // lista de classes
-
-    menu.classList.add('ativo');
-    menu.classList.add('ativo', 'mobile'); // duas classes
-
-    menu.classList.remove('ativo');
-    menu.classList.toggle('ativo'); // adiciona/remove a classe
-
-    
-    if(menu.classList.contains('ativo')) {
-        menu.classList.add("Possui-ativo")
-    }else {
-        menu.classList.add("Nao-Possui-ativo")
-    }
-    
-    // menu.classList.replace('ativo', 'inativo');
-    
