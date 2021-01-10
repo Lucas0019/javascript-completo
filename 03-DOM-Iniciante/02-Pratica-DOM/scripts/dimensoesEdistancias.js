a3778ad699f87c279baa8b3f153ef5f7a25a13f6
Lucas <lxavier.00@outlook.com>
2021-01-09 21:15:45 -0300

    Add .gitignore

--- 03-DOM-Iniciante/02-Pratica-DOM/scripts/dimensoesEdistancias.js
+++ 03-DOM-Iniciante/02-Pratica-DOM/scripts/dimensoesEdistancias.js
@@ -1,1 +1,6 @@
-//  Dimensões e Distâncias
+// Dimensões e Distâncias
+
+// Estas são propriedades e métodos dos objetos Element e HTMLElement, a maioria delas são Read Only
+const section = document.querySelector('.animais');
+
+
