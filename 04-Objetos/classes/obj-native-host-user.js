cfc47f278b57ef508f829f326a2632e09659e6cd
Lucas Xavier <lxvier.00@outlook.com>
2022-01-22 21:11:14 -0300

    feat: Added conceptual lesson on native object and host

---
+++ 04-Objetos/classes/obj-native-host-user.js
@@ -0,0 +1,26 @@
+// Native
+// Construtores de objetos nativos
+
+Object
+String
+Array
+Function
+Number
+Date
+
+// Host 
+// Implementado pelo própio ambiente , ex: Browser
+
+NodeList
+HTMLCollection
+Element
+Window
+
+
+// Verificar se Existe
+// O typeof retorna o tipo de dado. Caso esse dado não exista ou 
+// não tenha sido definido, ele irá retornar undefined. Ou seja, 
+// quando não for undefined quer dizer que existe.
+
+if (typeof Array.from !== "undefined")
+if (typeof NodeList !== "undefined");
